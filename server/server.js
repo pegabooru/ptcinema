2cd91d8d718acb8549c362058e1183feebd5763e
SimonPegasus <36254494+pegabooru@users.noreply.github.com>
2022-01-18 14:59:44 -0600

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,47 @@
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+var colors = require('colors');
+
+const app = express();
+
+app.use(express.static(`${__dirname}/../client`));
+
+const server = http.createServer(app);
+const io = socketio(server);
+var users = [];
+
+io.on('connection', (sock) => { // For each socket
+  users.push(sock.id);
+  sock.emit('clientid', sock.id); // Give player their id
+
+  sock.once('disconnect', function() { // When player disconnects
+    io.emit('exitserverplayer', sock.id);
+    users.pop(sock.id);
+  });
+
+  sock.on('clientmsg', (text) => { // Forward chat message to all players upon receiving from client
+    io.emit('servermsg', text);
+    console.log(text);
+  });
+
+  sock.on('askplayercount', () => { // Send player count
+    sock.emit('playercount', users.length);
+  });
+
+
+
+  sock.on('localplayerupdate', (data) => { // Receive and send local player updates to all players
+    io.emit('serverplayerupdate', data);
+    sock.emit('serverplayerdata', data);
+  });
+});
+
+const port = 80 // Server port
+server.on('error', (err) => { // On error
+  console.error(err);
+});
+server.listen(port, () => { // On start
+  console.log(colors.blue.bold('Ponytown Cinema'));
+  console.log(colors.green.bold('Listening on port '+port));
+});
